21abab42c17db2c5c95d6a72426c05b785dce696
kamal-hussain-75 <kamalhussain43555@gmail.com>
2025-12-26 15:55:54 +0530

    Add files via upload

---
+++ SearchProject.js
@@ -0,0 +1,162 @@
+let  users = [
+  {
+    name: "amisha rathore",
+    pic: "https://i.pinimg.com/736x/cd/9b/1c/cd9b1cf5b96e8300751f952488d6c002.jpg",
+    bio: "silent chaos in a loud world 🌙 | not for everyone"
+  },
+  {
+    name: "kiara mehta",
+    pic: "https://i.pinimg.com/736x/1f/2f/85/1f2f856bf3a020ed8ee9ecb3306ae074.jpg",
+    bio: "main character energy ✨ | coffee > everything ☕"
+  },
+  {
+    name: "isha oberoi",
+    pic: "https://images.unsplash.com/photo-1766469284258-11bf4223e2af?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "walking through dreams in doc martens 🖤 | late night thinker"
+   },
+  {
+    name: "ojin oklawa",
+    pic: "https://images.unsplash.com/photo-1632242219449-9f5d8b7c63f3?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "too glam to give a damn 💅 | filter free soul"
+  },
+  {
+    name: "ananya singh",
+    pic: "https://plus.unsplash.com/premium_photo-1661591155902-e0d1fe1523fe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "soft heart, savage mind 🌸"
+  },
+  {
+    name: "riya sharma",
+    pic: "https://plus.unsplash.com/premium_photo-1661777437775-97a30fc4baa0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "creating my own sunshine ☀️"
+  },
+  {
+    name: "zoya khan",
+    pic: "https://plus.unsplash.com/premium_photo-1661693445273-0aefa6ae28b7?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "moon child 🌙 | old soul"
+  },
+  {
+    name: "aarav malhotra",
+    pic: "https://images.unsplash.com/photo-1695266392553-9d38e27146b5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "dream big. hustle harder 🚀"
+  },
+  {
+    name: "kabir verma",
+    pic: "https://images.unsplash.com/photo-1566116386886-46622124e514?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "living one playlist at a time 🎧"
+  },
+  {
+    name: "neha joshi",
+    pic: "https://plus.unsplash.com/premium_photo-1669243451679-f65d5146e38c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "simple girl, complex thoughts ✨"
+  },
+  {
+    name: "aditya raj",
+    pic: "https://images.unsplash.com/photo-1629185752863-ec7c7f192c50?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "code. coffee. repeat 💻☕"
+  },
+  {
+    name: "pallavi desai",
+    pic: "https://plus.unsplash.com/premium_photo-1666298862782-91c1593a8289?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "art in chaos 🎨"
+  },
+  {
+    name: "rohan gupta",
+    pic: "https://images.unsplash.com/photo-1562439214-ba850d300049?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "minimal talks, maximum dreams"
+  },
+  {
+    name: "sneha kapoor",
+    pic: "https://plus.unsplash.com/premium_photo-1723629738845-f4d32a52ad62?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "grace in her heart 💫"
+  },
+  {
+    name: "yash patel",
+    pic: "https://images.unsplash.com/photo-1693138848894-14183bfa9b5b?q=80&w=998&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "focused. fearless. free."
+  },
+  {
+    name: "mehak arora",
+    pic: "https://plus.unsplash.com/premium_photo-1661433077395-b7a0889aa1e7?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "soft smile, strong soul 🌷"
+  },
+  {
+    name: "harsh singh",
+    pic: "https://images.unsplash.com/photo-1650965238931-8f642566fb57?q=80&w=1343&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "built from patience 🧠"
+  },
+  {
+    name: "simran kaur",
+    pic: "https://plus.unsplash.com/premium_photo-1661433091891-cc2532c188f0?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "kindness is power 💖"
+  },
+  {
+    name: "naman jain",
+    pic: "https://images.unsplash.com/photo-1668620222921-7249448d4b9b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "learning everyday 📚"
+  },
+  {
+    name: "alisha roy",
+    pic: "https://plus.unsplash.com/premium_photo-1661382126870-4ac4103ff87a?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "lost in aesthetics 🌸"
+  }
+];
+
+
+function showUsers(arr) {
+    arr.forEach(function (user) {
+
+        // card div
+        const card = document.createElement("div");
+        card.classList.add ("card");
+
+       // image
+       const img = document.createElement("img");
+       img.src = user.pic; // ya actual image path
+       img.classList.add("bg-img");
+
+       // blurred layer
+       const blurLayer = document.createElement("div");
+       blurLayer.style.backgroundImage = `url(${user.pic})`; // ya actual image path
+       blurLayer.classList.add("blurred-layer");
+
+       // content div
+       const content = document.createElement("div");
+       content.classList.add("content");
+
+       // h3
+       const heading = document.createElement("h3");
+       heading.textContent = user.name;
+
+       // p
+       const para = document.createElement("p");
+       para.textContent = user.bio;
+
+       // append content ke andar
+       content.appendChild(heading);
+       content.appendChild(para);
+
+       // append card ke andar
+       card.appendChild(img);
+       card.appendChild(blurLayer);
+       card.appendChild(content);
+
+       // finally body me add
+        document.querySelector("#cardss").appendChild(card);
+      
+
+  
+    });
+}
+
+showUsers(users);
+
+let inp=document.querySelector(".inp");
+inp.addEventListener("input",function(){
+    let newUser=users.filter((user)=>{
+        return user.name.startsWith(inp.value);
+    });
+    document.querySelector("#cardss").innerHTML="";
+  showUsers(newUser);
+   
+});
+
